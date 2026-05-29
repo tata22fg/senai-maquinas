fc9d76cf9ddc2585918e90d7f8f5d773b61d86f0
tata22fg <emanuelbatista389@gmail.com>
2026-05-29 03:14:48 -0300

    fix mysql railway

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  schema: "./server/drizzle/schema.ts",
+  schema: "./drizzle/schema.ts",
   out: "./drizzle",
   dialect: "mysql",
   dbCredentials: {
